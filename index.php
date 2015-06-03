9cc4482c3b7eb3adbdaf6539d490173f2510a5be
Macattack <marcus_1234_@hotmail.com>
2015-06-03 22:31:42 +0200

    Flex style 3 columns

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-phpinfo();
 
-echo "Hej smarthome";
 
 ?>
 <!DOCTYPE html>
@@ -9,11 +7,49 @@ echo "Hej smarthome";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    
+    <title>Smart home - Kebab edition</title>
+    
     <link rel="stylesheet" href="css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:500,300,400|Open+Sans:300,400' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
+    <div class="container">
+        <div class="column column-1">
+            <div id="time" class="content">
+                <div class="header">
+                    12:00:31<br/>
+                    1 Juni 2035
+                </div>
+            </div>
+            <div id="weather" class="content">
+                <div class="header">
+                    35 grader<br/>
+                    Sol hela dagen
+                </div>
+            </div>          
+        </div>
+        
+        <div class="column column-2">
+            <div id="news" class="content">
+                <div class="header">
+                    News
+                </div>
+            </div>  
+        </div>
+        
+        <div class="column column-3">
+            <div id="news" class="content">
+                <div class="header">
+                    News
+                </div>
+            </div>  
+        </div>
+
+        
+    </div>
+    
     <script src="js/global.js"></script>
 </body>
 </html>
