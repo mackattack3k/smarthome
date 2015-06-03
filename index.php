ce734b6aab9df15133b798cf59b251bd5286b17d
Macattack <marcus_1234_@hotmail.com>
2015-06-03 20:18:41 +0200

    no message

--- index.php
+++ index.php
@@ -4,3 +4,16 @@ phpinfo();
 echo "Hej smarthome";
 
 ?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <script src="js/global.js"></script>
+</body>
+</html>
