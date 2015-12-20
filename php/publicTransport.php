6bbf4d768b882e998e1fde332542bfc6692f6a17
Macattack <marcus_1234_@hotmail.com>
2015-12-20 21:52:18 +0100

    Traffic now works with new publicTransport class.

--- php/publicTransport.php
+++ php/publicTransport.php
@@ -9,25 +9,18 @@
 class publicTransport
 {
 
-    public $lat;
-
     /*
      * Units
      */
-    public $lon;
-    public $measurements;
-    public $lang;
+
     public $debug;
+    public $inputStationID;
     public $allUnits = array();
     public $API_KEYS = array();
 
     public function __construct()
     {
-        $function = isset($_GET['function']) ? $_GET['function'] : false;
-        $debug = isset($_GET['debug']) ? $_GET['debug'] : false;
-
-        $this->setDebug($debug);
-
+        //TODO: Make this a function instead
         if (file_exists(__DIR__ . '/../../api_keys.php')) {
             global $GlobalAPI_Keys;
             require_once(__DIR__ . '/../../api_keys.php');
@@ -37,11 +30,15 @@ class publicTransport
             return;
         }
 
+        $function = isset($_GET['function']) ? $_GET['function'] : false;
+        $debug = isset($_GET['debug']) ? $_GET['debug'] : false;
+        $inputStationID = isset($_GET['stationid']) ? $_GET['stationid'] : false;
+
+        $this->setDebug($debug);
+        $this->setInputStationID($inputStationID);
+
         if ($function == "fullHTML") {
-            //echo "starting html version";
-            $htmlOutput = $this->startHTMLAutomaticVersion();
-            echo $htmlOutput;
-            return;
+            $this->startHTMLAutomaticVersion();
         }
 
     }
@@ -78,133 +75,162 @@ class publicTransport
         }
     }
 
-    function findStation($inputStationName){
+    public function setInputStationID($inputStationID)
+    {
+        $this->inputStationID = $inputStationID;
+    }
+
+    public function getInputStationID()
+    {
+        return $this->inputStationID;
+    }
+
+    private function startHTMLAutomaticVersion()
+    {
+        $stationID = $this->getInputStationID();
+        if ( $stationID ){
+
+            $stationData = $this->getDepartures($stationID);
+            if ( $stationData ){
+                $stationHTMLData = $this->convertDeparturesToHtml($stationData);
+                return $stationHTMLData;
+            }
+            echo "Error: No data returned from Resrobot";
+            return false;
+        }
+        echo "Error: startHTMLAutomaticVersion:: stationid is not set";
+    }
+
+    //TODO: remove this and use rawurlencode in getstationsfromSL
+    function findStation($inputStationName)
+    {
         if ($inputStationName == '') {
             echo "No station name entered";
             return;
         }
 
-        echo "Finding station that matches: ".$inputStationName."<br/>";
+        echo "Finding station that matches: " . $inputStationName . "<br/>";
         $urlStationName = rawurlencode($inputStationName);
         //Get json result from SL
         $stations = getStationsFromSL($urlStationName);
     }
 
-    function getStationsFromSL($stationName){
-        global $searchTripApiKey;
+    function getStationsFromSL($stationName)
+    {
         global $platsuppslagKey;
-        //$findByName             =   "https://api.trafiklab.se/samtrafiken/resrobot/FindLocation.json?apiVersion=2.1&from=$stationName&coordSys=RT90&key=$searchTripApiKey";
-        $findByName             =   "https://api.sl.se/api2/typeahead.json?key=$platsuppslagKey&searchstring=$stationName&stationsonly=TRUE&maxresults=5";
-        $findByNameResult       =   file_get_contents($findByName);
-        $findByNameResultJson   =   json_decode($findByNameResult, true);
+        $resrobotSearchKey = $this->getAPIKEYS()['resrobotSearchKey'];
+        $resrobot= "https://api.resrobot.se/location.name?key=$resrobotSearchKey&input=$stationName&format=json";
+        $findByName = "https://api.sl.se/api2/typeahead.json?key=$platsuppslagKey&searchstring=$stationName&stationsonly=TRUE&maxresults=5";
+        $findByNameResult = file_get_contents($findByName);
+        $findByNameResultJson = json_decode($findByNameResult, true);
         print_r($findByNameResultJson);
-        $stations               =   $findByNameResultJson->ResponseData;
+        $stations = $findByNameResultJson->ResponseData;
 
         //Check if there isnt any results for the station
         if (count($stations) < 1) {
             echo "No results";
             return;
         }
-        /*echo "<pre>";
-        print_r(($station));
-        echo "</pre>";
-        */
-
-        //Return the results
-        /*if ( !array_key_exists(0, $stations) ) {
-          echo $stations['Name']." - ".$stations['SiteId'];
-          return;
-        }
-        */
         foreach ($stations as $key => $station) {
-            echo $station['Name']." - ".$station['SiteId']."<br/>";
+            echo $station['Name'] . " - " . $station['SiteId'] . "<br/>";
         }
         return;
 
 
     }
 
-    function getDepartures($inputSiteID){
-        //echo $getOrigin."<br>".$getDestination."<br>".$date."<br>".$time."<br>";
-        global $realtidsInformation3; //Get the api key from apikeys.php
-        global $resrobotKey;
-        $amanningevagen       =     '7453026'; //Åmänningevägen
-        $arstaberg            =     '7424920'; //Årstaberg station
-        $gullmarsplan         =     '7421705'; //Gullmarsplan
+    function getDepartures($inputSiteID)
+    {
+        $resrobotKey = $this->getAPIKEYS()['resrobotKey'];
+        $output = null;
+
         /*
-        * ULT = Tunnelbana, BLT = Buss, SLT = Tvärbana
-        */
+         * ULT = Tunnelbana, BLT = Buss, SLT = Tvärbana
+         * Åmänningevägen = 7453026, Årstaberg station = 7424920, Gullmarsplan = 7421705
+         *
+         */
 
-        if($inputSiteID){
-            $findByStationIDOLD        = "https://api.sl.se/api2/realtimedepartures.json?key=".$realtidsInformation3."&siteid=".$inputSiteID."&timewindow=30";
-            $findByStationID       = "https://api.resrobot.se/departureBoard?key=".$resrobotKey."&id=7453026&maxJourneys=10&format=json";
-            $debug = 0;
-            $transportTypeTranslationArray = new stdClass();
-            $transportTypeTranslationArray->ULT = 'train'; //Tunnelbana
-            $transportTypeTranslationArray->SLT = 'train'; //Tvärbana
-            $transportTypeTranslationArray->BLT = 'bus'; //Buss
+        if (!$inputSiteID) {
+            $output = "Error: No site ID in getDepartures()";
+            return $output;
+        }
+        $findByStationID = "https://api.resrobot.se/departureBoard?key=$resrobotKey&id=$inputSiteID&maxJourneys=10&format=json";
 
-            $departsResultJson = json_decode(@file_get_contents($findByStationID));
-            $departsResult = $departsResultJson->Departure;
+        $departsResultJson = @file_get_contents($findByStationID);
+        $departsResultJsonArray = json_decode($departsResultJson, true);
+        $departsResultJsonObj = json_decode($departsResultJson);
 
-            foreach ($departsResult as $departureArrayKey => $departureInfo) {
-                //var_dump($departureInfo);
-                //exit this departure loop if the bus is going to the same station as we are going from (Weird bug from api call)
-                if ($departureInfo->Product->num == '') {
-                    continue;
-                }
+        if( empty( $departsResultJsonArray ) )
+        {
+            return false;
+        }
+        return $departsResultJsonObj;
+    }
 
-                $stops = $departureInfo->Stops->Stop;
-                end($stops);         // move the internal pointer to the end of the array
-                $lastStopObject = current($stops);
-
-                $transportationCategory = $departureInfo->transportCategory;
-
-                /*
-                * Final values
-                */
-
-                $arrivalStopName = preg_replace('/\s\(.*\)?/', '', $departureInfo->direction);
-                $arrivalTime = substr($lastStopObject->arrTime, 0,5); //Cutting string since I only need HH:MM
-                $departTime = substr($departureInfo->time, 0,5);
-                $departDate = $departureInfo->date;
-                $lineFullName = $departureInfo->Product->name;
-                $line = substr($lineFullName, 4, strlen($arrivalStopName));
-
-                //Check if the transportation type exists in our translation array and then translate it
-                if(isset($transportTypeTranslationArray->$transportationCategory)){
-                    $translatedTransportType = $transportTypeTranslationArray->$transportationCategory;
-                } else {
-                    $translatedTransportType = 'rocket';
-                }
-
-                /*
-                * Output the HTML
-                */
-                echo "
-              <div class='traffic-result'>
-                <div class='traffic-first'>
-                  <div class='icon icon-".strtolower($translatedTransportType)." icon-2x'></div>
-                  <div class='traffic-line'>".$line."</div>
-                </div>
-                <div class='traffic-second'>
-                  <div class='traffic-destination'>".$arrivalStopName."</div>
-                </div>
-                <div class='traffic-third'>
-                  <div class='traffic-time departure-time' value='".$departDate." ".$departTime."'>avgår ".$departTime."</div>
-                  <div class='traffic-time arrival-time'>framme ".$arrivalTime."</div>
-                </div>
-              </div>
-          ";
-
-
-            }
-
-
-
-
+    private function convertDeparturesToHtml($departsResultJson){
+        $output = null;
+        if ( !$departsResultJson ){
+            $output .= "Error: No data in convertDeparturesToHtml";
+        }
+        if ( $this->getDebug() == "true" ){
+            $output .= print_r($departsResultJson);
         }
 
+        $departsResult = $departsResultJson->Departure;
+
+        $transportTypeTranslationArray = new stdClass();
+        $transportTypeTranslationArray->ULT = 'train'; //Tunnelbana
+        $transportTypeTranslationArray->SLT = 'train'; //Tvärbana
+        $transportTypeTranslationArray->BLT = 'bus'; //Buss
+
+        foreach ($departsResult as $departureArrayKey => $departureInfo) {
+            //exit this departure loop if the bus is going to the same station as we are going from (Weird bug from api call)
+            if ($departureInfo->Product->num == '') {
+                continue;
+            }
+
+            $stops = $departureInfo->Stops->Stop;
+            end($stops);         // move the internal pointer to the end of the array
+            $lastStopObject = current($stops);
+
+            $transportationCategory = $departureInfo->transportCategory;
+
+            /*
+            * Final values
+            */
+
+            $arrivalStopName = preg_replace('/\s\(.*\)?/', '', $departureInfo->direction);
+            $arrivalTime = substr($lastStopObject->arrTime, 0, 5); //Cutting string since I only need HH:MM
+            $departTime = substr($departureInfo->time, 0, 5);
+            $departDate = $departureInfo->date;
+            $lineFullName = $departureInfo->Product->name;
+            $line = substr($lineFullName, 4, strlen($arrivalStopName));
+
+            //Check if the transportation type exists in our translation array and then translate it
+            if (isset($transportTypeTranslationArray->$transportationCategory)) {
+                $translatedTransportType = $transportTypeTranslationArray->$transportationCategory;
+            } else {
+                $translatedTransportType = 'rocket';
+            }
+
+            /*
+            * Output the HTML
+            */
+            $output .=  "<div class='traffic-result'>";
+            $output .=  "<div class='traffic-first'>";
+            $output .=  "<div class='icon icon-".strtolower($translatedTransportType)." icon-2x'></div>";
+            $output .=  "<div class='traffic-line'>$line</div>";
+            $output .=  "</div>";
+            $output .=  "<div class='traffic-second'>";
+            $output .=  "<div class='traffic-destination'>" . $arrivalStopName . "</div>";
+            $output .=  "</div>";
+            $output .=  "<div class='traffic-third'>";
+            $output .=  "<div class='traffic-time departure-time' value='$departDate $departTime'>avgår $departTime</div>";
+            $output .=  "<div class='traffic-time arrival-time'>framme $arrivalTime</div>";
+            $output .=  "</div>";
+            $output .=  "</div>";
+        }
+        return $output;
     }
 
 }
