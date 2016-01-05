5bb5c5708962b6cd87d726cd9577318000f0a5f5
Macattack <marcus_1234_@hotmail.com>
2016-01-05 01:28:42 +0100

    Styled left slideout menu

--- index.php
+++ index.php
@@ -26,11 +26,22 @@ require_once ('php/lights.php');
 
     <!-- Polymer -->
     <link rel="import" href="bower_components/paper-toggle-button/paper-toggle-button.html">
-    <link rel="import" href="bower_components/paper-toggle-button/paper-toggle-button.css">
     <link rel="import" href="bower_components/paper-button/paper-button.html">
     <link rel="import" href="bower_components/paper-checkbox/paper-checkbox.html">
     <link rel="import" href="bower_components/paper-styles/paper-styles.html">
     <link rel="import" href="bower_components/paper-input/paper-input.html">
+    <link rel="import" href="bower_components/paper-dropdown-menu/paper-dropdown-menu.html">
+    <link rel="import" href="bower_components/paper-listbox/paper-listbox.html">
+    <link rel="import" href="bower_components/paper-item/paper-item.html">
+    <link rel="import" href="bower_components/paper-tooltip/paper-tooltip.html">
+    <link rel="import" href="bower_components/iron-a11y-keys-behavior/iron-a11y-keys-behavior.html">
+    <link rel="import" href="bower_components/iron-dropdown/iron-dropdown.html">
+    <link rel="import" href="bower_components/iron-behaviors/iron-control-state.html">
+    <link rel="import" href="bower_components/iron-behaviors/iron-button-state.html">
+    <link rel="import" href="bower_components/iron-dropdown/iron-dropdown.html">
+
+
+
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-touch-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-touch-icon-60x60.png">
@@ -54,38 +65,74 @@ require_once ('php/lights.php');
 </head>
 
 <body>
-<nav id="settings">
-    <h2>Settings</h2>
-    <section class="settings-section">
-        <!--<h3 class="settings-section-title">Docs</h3>-->
-        <ul class="settings-section-list">
-            <style is="custom-style">
-                paper-input {
-                    --paper-input-container-color: #C2185B;
-                    --paper-input-container-focus-color: #C2185B;
-                    --paper-input-container-invalid-color: rgba(194, 24, 91, 0.25);
-                    --paper-input-container-input-color: black;
-                }
-                paper-toggle-button {
-                    --paper-checkbox-checked-color: #C2185B;
-                    --paper-checkbox-unchecked-background-color: white;
-                    --paper-checkbox-checked-ink-color: #C2185B;
-                    --paper-checkbox-unchecked-color: white;
-                    --paper-checkbox-unchecked-ink-color: #C2185B;
-                }
-            </style>
-            <li><paper-input
-                    pattern="^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$"
-                    label="Latitude"
-                    error="Not a correct coordinate"
-                    onfocusout="validate()"></paper-input></li>
-            <li><paper-input label="Longitude"></paper-input></li>
-            <li><paper-input label="Stocks"></paper-input></li>
-            <li><label for="notifications-toggle">Notifications</label></li>
-            <li><paper-toggle-button id="notifications-toggle">Nooooooots</paper-toggle-button></li>
-            <li><paper-input disabled label="Language"></paper-input></li>
-        </ul>
-    </section>
+<nav id="slide-menu">
+    <div id="settings">
+        <div class="settings-header">Settings</div>
+        <section class="settings-section">
+            <!--<h3 class="settings-section-title">Docs</h3>-->
+            <ul class="settings-section-list">
+                <style is="custom-style">
+                    paper-input {
+                        --paper-input-container-color: #C2185B;
+                        --paper-input-container-focus-color: #C2185B;
+                        --paper-input-container-invalid-color: rgba(194, 24, 91, 0.25);
+                        --paper-input-container-input-color: black;
+                    }
+                    paper-toggle-button {
+                        --paper-toggle-button-checked-bar-color: #C2185B;
+                        --paper-toggle-button-checked-ink-color: white;
+                        --paper-toggle-button-checked-button-color: #C2185B;
+                        --paper-toggle-button-label-color: #C2185B;
+                        margin-top: 20px;
+                    }
+                    paper-dropdown-menu {
+                        --paper-input-container-color: #C2185B;
+                        --paper-input-container-focus-color: #C2185B;
+
+                    }
+                </style>
+                <li><paper-input
+                        label="Stocks"
+                        pattern="^([a-zA-Z]{2,6})+(,[a-zA-Z]{2,6})*$"
+                        onfocusout="validate()">
+                    </paper-input>
+                    <paper-tooltip position="bottom" offset="10">eg AAPL,GOOG,MSFT</paper-tooltip>
+                </li>
+                <li><paper-input
+                        label="Number of departures"
+                        pattern="^\d*$"
+                        onfocusout="validate()"></paper-input></li>
+                <li><paper-dropdown-menu label="Station">
+                        <paper-menu class="dropdown-content">
+                            <paper-item>Gullmarsplan</paper-item>
+                            <paper-item>Sulvägen</paper-item>
+                            <paper-item>Årstaberg</paper-item>
+                            <paper-item>Åmänningevägen</paper-item>
+                        </paper-menu>
+                    </paper-dropdown-menu></li>
+                <li><paper-input
+                        label="Latitude"
+                        pattern="^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$"
+                        onfocusout="validate()"></paper-input></li>
+                <li><paper-input
+                        label="Longitude"
+                        pattern="^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$"
+                        onfocusout="validate()"></paper-input></li>
+                <li><paper-input
+                        label="Timezone"
+                        pattern="^\d*$"
+                        onfocusout="validate()"></paper-input></li>
+                <li><paper-toggle-button
+                        class="hover no-select"
+                        id="notifications-toggle">Notifications</paper-toggle-button></li>
+                <li><paper-toggle-button
+                        class="hover no-select"
+                        id="debug-toggle">Debug</paper-toggle-button></li>
+                <li><paper-input disabled label="Language"></paper-input></li>
+                <!--TODO: Create checkboxes to enable or disable columns/info-->
+            </ul>
+        </section>
+    </div>
 </nav>
 
 <div id="panel" class="column-container">
@@ -121,6 +168,7 @@ require_once ('php/lights.php');
     <div class="column column-2">
         <div id="traffic" class="column-content">
             <div class="traffic-refresh-manually icon icon-refresh icon-2x" id="refresh-traffic">
+                <!--TODO: Change this to paper-spinner-->
             </div>
             <div class="header">
                 Trafik
@@ -189,7 +237,7 @@ require_once ('php/lights.php');
 <script>
     var slideout = new Slideout({
         'panel': document.getElementById('panel'),
-        'menu': document.getElementById('settings'),
+        'menu': document.getElementById('slide-menu'),
         'padding': 260,
         'tolerance': 70
     });
